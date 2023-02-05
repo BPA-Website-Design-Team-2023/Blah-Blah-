dcbccb8a061fceadf585e6620cc1803a8d0ce96e
Dan Nguyen <77303944+dannguyen-1004@users.noreply.github.com>
2023-02-04 20:58:59 -0600

    test home page

--- Home/script.js
+++ Home/script.js
@@ -1,9 +1,7 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
+car =  document.getElementById('car_animation');
+
+car.addEventListener("timeupdate", function(){
+    if(this.currentTime >= 3.141) {
+        this.pause();
     }
-  }
-  
+});
