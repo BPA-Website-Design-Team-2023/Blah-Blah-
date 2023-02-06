9b2e6a115d5179cfbbcd3a84c5cccf286dc6451d
Dan Nguyen <77303944+dannguyen-1004@users.noreply.github.com>
2023-02-06 09:26:51 -0600

    homepage

--- Home/script.js
+++ Home/script.js
@@ -1,7 +1,20 @@
 car =  document.getElementById('car_animation');
-
 car.addEventListener("timeupdate", function(){
     if(this.currentTime >= 3.141) {
         this.pause();
     }
 });
+
+
+function appearAnimation(){
+    const element = document.getElementById("fourth");
+    const rect = element.getBoundingClientRect();
+    if(rect.top.toFixed() <= 200){
+       $("#dissapear").fadeOut(5000);
+       $("#appear").fadeIn(5000);
+       if(innerWidth >= 850){
+        document.getElementById("appear").style.display = "flex";
+       }
+       
+    }
+}
